5712b72ee220349a7724cec0b11cffa089a3bb23
MariaManuela <pmariamanuela@yahoo.com>
2019-03-14 15:56:48 +0200

    pedobear

--- app/src/main/java/com/calculmobil/calculmobil/ProgressBarMenu.java
+++ app/src/main/java/com/calculmobil/calculmobil/ProgressBarMenu.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
 public class ProgressBarMenu extends AppCompatActivity {
 
     private ProgressBar progressBar;
+    ImageButton calendarIcon;
+    private Button startButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedometer);
-
+        calendarIcon = (ImageButton) findViewById(R.id.calendarButton);
+        startButton = (Button) findViewById(R.id.startPauseButton);
         progressBar.setVisibility(View.VISIBLE);
         init();
         new Handler().post(new Runnable() {
